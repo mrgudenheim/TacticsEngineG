7709dc8af4720d451e3fefa14160aa47bf5d44c6
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-03-14 21:54:57 -0400

    made formatting in Shp align with style guide

--- src/file_formats/Shp.gd
+++ src/file_formats/Shp.gd
@@ -105,29 +105,29 @@ const constant_sp2_v_offsets: Dictionary[int, int] = { # spritesheet with sp2s a
 	}
 
 
-var file_name:String = "default_file_name"
-var name_alias:String = "default_name_alias"
+var file_name: String = "default_file_name"
+var name_alias: String = "default_name_alias"
 
 # section 1
-var swim_pointer:int = 0
-var attack_start_index:int = 9999
-var sp_extra:int = 0
-var zero_frames:Array[int] = [] # only used in wep or eff
-var section1_length:int = 0:
+var swim_pointer: int = 0
+var attack_start_index: int = 9999
+var sp_extra: int = 0
+var zero_frames: Array[int] = [] # only used in wep or eff
+var section1_length: int = 0:
 	get:
 		return 0x44 if (file_name.begins_with("WEP") or file_name.begins_with("EFF")) else 8
 
 # section 2
-var frame_pointers:Array[int] = []
-var section2_length:int = 0:
+var frame_pointers: Array[int] = []
+var section2_length: int = 0:
 	get:
 		return 0x800 if (file_name.begins_with("WEP") or file_name.begins_with("EFF")) else 0x400
 	
 # section 3
 var frames: Array[FrameData] = []
-var section3_length:int = 0:
+var section3_length: int = 0:
 	get:
-		var sum:int = 0
+		var sum: int = 0
 		for frame in frames:
 			sum += frame.length
 		return sum + 2 # bytes
@@ -262,15 +262,15 @@ func _get_frame_data(bytes: PackedByteArray) -> FrameData:
 	frame_data.byte2_4 = (bytes.decode_u8(1) & 0x80) >> 7
 	
 	for subframe_index: int in frame_data.num_subframes:
-		var subframe_start:int = 2 + (subframe_index * 4)
-		var subframe_bytes:PackedByteArray = bytes.slice(subframe_start, subframe_start + 4)
+		var subframe_start: int = 2 + (subframe_index * 4)
+		var subframe_bytes: PackedByteArray = bytes.slice(subframe_start, subframe_start + 4)
 		frame_data.subframes.append(_get_subframe_data(subframe_bytes))
 	
 	return frame_data
 
 
-func _get_subframe_data(bytes:PackedByteArray) -> SubFrameData:
-	var subframe:SubFrameData = SubFrameData.new()
+func _get_subframe_data(bytes: PackedByteArray) -> SubFrameData:
+	var subframe: SubFrameData = SubFrameData.new()
 	
 	subframe.shift_x = bytes.decode_s8(0)
 	subframe.shift_y = bytes.decode_s8(1)
@@ -278,7 +278,7 @@ func _get_subframe_data(bytes:PackedByteArray) -> SubFrameData:
 	var bytes23: int = bytes.decode_u16(2)
 	subframe.load_location_x = (bytes23 & 0x001F) * PIXELS_PER_TILE # pixels
 	subframe.load_location_y = ((bytes23 & 0x03E0) >> 5) * PIXELS_PER_TILE # pixels
-	var rect_size_index:int = (bytes23 & 0x3C00) >> 10
+	var rect_size_index: int = (bytes23 & 0x3C00) >> 10
 	subframe.rect_size = SUBFRAME_RECT_SIZES[rect_size_index] # pixels
 	subframe.flip_x = (bytes23 & 0x4000) != 0
 	subframe.flip_y = (bytes23 & 0x8000) != 0
@@ -287,8 +287,8 @@ func _get_subframe_data(bytes:PackedByteArray) -> SubFrameData:
 
 
 func write_shp() -> void:
-	var bytes:PackedByteArray = []
-	var total_size:int = section1_length + section2_length + section3_length
+	var bytes: PackedByteArray = []
+	var total_size: int = section1_length + section2_length + section3_length
 	if has_submerged_data:
 		total_size += section2_length + frames_submerged_length
 	bytes.resize(total_size)
@@ -297,7 +297,7 @@ func write_shp() -> void:
 	# section 1
 	# wep and eff shapes have a slightly different format
 	if (file_name.begins_with("WEP") or file_name.begins_with("EFF")):
-		var initial_offset:int = 6
+		var initial_offset: int = 6
 		
 		# skip zero bytes and unarmed
 		bytes.encode_u8(0, 0)
@@ -307,7 +307,7 @@ func write_shp() -> void:
 		bytes.encode_u8(4, 0)
 		bytes.encode_u8(5, 0)
 				
-		for zero_frame_index:int in zero_frames.size():
+		for zero_frame_index: int in zero_frames.size():
 			bytes.encode_u16(initial_offset + (zero_frame_index*2), zero_frames[zero_frame_index])
 	else:
 		bytes.encode_u32(0, swim_pointer)
@@ -323,26 +323,26 @@ func write_shp() -> void:
 	save_file.store_buffer(bytes)
 
 
-func _write_sections23_bytes(bytes:PackedByteArray,  data_start_pointer:int, pointers:Array[int], frames_list:Array[FrameData], section3data_length:int) -> PackedByteArray:
+func _write_sections23_bytes(bytes: PackedByteArray,  data_start_pointer: int, pointers: Array[int], frames_list: Array[FrameData], section3data_length: int) -> PackedByteArray:
 	# section 2
-	for frame_index:int in pointers.size():
+	for frame_index: int in pointers.size():
 		bytes.encode_u32(data_start_pointer + (frame_index * 4), pointers[frame_index])	
 	
 	# section 3
 	bytes.encode_u16(data_start_pointer + section2_length, section3data_length)
 	
 	var frame_data_start_pointer: int = data_start_pointer + section2_length + 2
-	for frame_index:int in pointers.size():
-		var frame_offset:int = frame_data_start_pointer + pointers[frame_index]
-		var frame_data:FrameData = frames_list[frame_index]
-		var b1:int = frame_data.num_subframes - 1
-		var b1_2:int = ROTATIONS_DEGREES.find(frame_data.y_rotation) << 3
+	for frame_index: int in pointers.size():
+		var frame_offset: int = frame_data_start_pointer + pointers[frame_index]
+		var frame_data: FrameData = frames_list[frame_index]
+		var b1: int = frame_data.num_subframes - 1
+		var b1_2: int = ROTATIONS_DEGREES.find(frame_data.y_rotation) << 3
 		b1 = b1 | b1_2
 		
-		var b2:int = (frame_data.transparency_flag as int) & 0b01
-		var b2_2:int = frame_data.byte2_3 << 1
-		var b2_3:int = frame_data.transparency_type << 5
-		var b2_4:int = frame_data.byte2_4 << 7
+		var b2: int = (frame_data.transparency_flag as int) & 0b01
+		var b2_2: int = frame_data.byte2_3 << 1
+		var b2_3: int = frame_data.transparency_type << 5
+		var b2_4: int = frame_data.byte2_4 << 7
 		b2 = b2 | b2_2
 		b2 = b2 | b2_3
 		b2 = b2 | b2_4
@@ -351,12 +351,12 @@ func _write_sections23_bytes(bytes:PackedByteArray,  data_start_pointer:int, poi
 		bytes.encode_u8(frame_offset + 1, b2)
 		
 		for subframe_index in frame_data.subframes.size():
-			var subframe:SubFrameData = frame_data.subframes[subframe_index]
-			var b56:int = (subframe.load_location_x / PIXELS_PER_TILE)
-			var b56_2:int = (subframe.load_location_y / PIXELS_PER_TILE) << 5
-			var b56_3:int = SUBFRAME_RECT_SIZES.find(subframe.rect_size) << 10
-			var b56_4:int = (subframe.flip_x as int) << 14
-			var b56_5:int = (subframe.flip_y as int) << 15
+			var subframe: SubFrameData = frame_data.subframes[subframe_index]
+			var b56: int = (subframe.load_location_x / PIXELS_PER_TILE)
+			var b56_2: int = (subframe.load_location_y / PIXELS_PER_TILE) << 5
+			var b56_3: int = SUBFRAME_RECT_SIZES.find(subframe.rect_size) << 10
+			var b56_4: int = (subframe.flip_x as int) << 14
+			var b56_5: int = (subframe.flip_y as int) << 15
 			
 			b56 = b56 | b56_2
 			b56 = b56 | b56_3
@@ -370,8 +370,8 @@ func _write_sections23_bytes(bytes:PackedByteArray,  data_start_pointer:int, poi
 	return bytes
 
 
-func set_data_from_cfg(filepath:String) -> void:
-	var cfg:ConfigFile = ConfigFile.new()
+func set_data_from_cfg(filepath: String) -> void:
+	var cfg: ConfigFile = ConfigFile.new()
 	var err := cfg.load(filepath)
 	#var err = cfg.load("user://FFTorama/"+file_name+"_shp.cfg")
 	
@@ -399,7 +399,7 @@ func _get_frames_from_cfg(cfg: ConfigFile, pointers: Array[int], cfg_file_name: 
 	var temp_frames: Array[FrameData] = []
 	
 	for frame_index in pointers.size():
-		var frame_label:String = cfg_file_name + "-" + str(frame_index)
+		var frame_label: String = cfg_file_name + "-" + str(frame_index)
 		temp_frames.append(FrameData.new())
 		temp_frames[frame_index].num_subframes = cfg.get_value(frame_label, "num_subframes")
 		temp_frames[frame_index].y_rotation = cfg.get_value(frame_label, "y_rotation")
@@ -409,7 +409,7 @@ func _get_frames_from_cfg(cfg: ConfigFile, pointers: Array[int], cfg_file_name: 
 		temp_frames[frame_index].byte2_4 = cfg.get_value(frame_label, "byte2_4")
 		
 		for subframe_index in temp_frames[frame_index].num_subframes:
-			var subframe_label:String = frame_label + "-" + str(subframe_index)
+			var subframe_label: String = frame_label + "-" + str(subframe_index)
 			temp_frames[frame_index].subframes.append(SubFrameData.new())
 			temp_frames[frame_index].subframes[subframe_index].shift_x = cfg.get_value(subframe_label, "shift_x")
 			temp_frames[frame_index].subframes[subframe_index].shift_y = cfg.get_value(subframe_label, "shift_y")
@@ -423,7 +423,7 @@ func _get_frames_from_cfg(cfg: ConfigFile, pointers: Array[int], cfg_file_name: 
 
 
 func write_cfg() -> void:
-	var cfg:ConfigFile = ConfigFile.new()
+	var cfg: ConfigFile = ConfigFile.new()
 	cfg.set_value(file_name, "file_name", file_name)
 	cfg.set_value(file_name, "name_alias", name_alias)
 	cfg.set_value(file_name, "swim_pointer", swim_pointer)
@@ -440,10 +440,10 @@ func write_cfg() -> void:
 	cfg.save("user://FFTorama/"+file_name+"_shp.cfg")
 
 
-func _write_cfg_frames(cfg:ConfigFile, temp_frames:Array[FrameData], cfg_file_name:String) -> ConfigFile:
-	for frame_index:int in temp_frames.size():
-		var frame_label:String = cfg_file_name + "-" + str(frame_index)
-		var frame_data:FrameData = temp_frames[frame_index]
+func _write_cfg_frames(cfg: ConfigFile, temp_frames: Array[FrameData], cfg_file_name: String) -> ConfigFile:
+	for frame_index: int in temp_frames.size():
+		var frame_label: String = cfg_file_name + "-" + str(frame_index)
+		var frame_data: FrameData = temp_frames[frame_index]
 		cfg.set_value(frame_label, "num_subframes", frame_data.num_subframes)
 		cfg.set_value(frame_label, "y_rotation", frame_data.y_rotation)
 		cfg.set_value(frame_label, "transparency_type", frame_data.transparency_type)
@@ -456,10 +456,10 @@ func _write_cfg_frames(cfg:ConfigFile, temp_frames:Array[FrameData], cfg_file_na
 	return cfg
 
 
-func _write_cfg_subframes(cfg:ConfigFile, temp_frame_data:FrameData, frame_label:String) -> ConfigFile:
+func _write_cfg_subframes(cfg: ConfigFile, temp_frame_data: FrameData, frame_label: String) -> ConfigFile:
 	for subframe_index in temp_frame_data.num_subframes:
-		var subframe_label:String = frame_label + "-" + str(subframe_index)
-		var subframe_data:SubFrameData = temp_frame_data.subframes[subframe_index]
+		var subframe_label: String = frame_label + "-" + str(subframe_index)
+		var subframe_data: SubFrameData = temp_frame_data.subframes[subframe_index]
 		cfg.set_value(subframe_label, "shift_x", subframe_data.shift_x)
 		cfg.set_value(subframe_label, "shift_y", subframe_data.shift_y)
 		cfg.set_value(subframe_label, "load_location_x", subframe_data.load_location_x)
@@ -479,10 +479,10 @@ func set_frames_from_csv(filepath:String) -> void:
 	if file == null:
 		push_warning(FileAccess.get_open_error())
 		return
-	var frame_data_csv:String = file.get_as_text()
+	var frame_data_csv: String = file.get_as_text()
 	
 	if file_name == "default_file_name":
-		var new_file_name:String = filepath.get_file()
+		var new_file_name: String = filepath.get_file()
 		new_file_name = new_file_name.trim_suffix(".csv")
 		new_file_name = new_file_name.trim_suffix(".txt")
 		new_file_name = new_file_name.trim_prefix("frame_data_")
@@ -491,19 +491,19 @@ func set_frames_from_csv(filepath:String) -> void:
 		file_name = new_file_name
 		name_alias = new_file_name
 	
-	var frames_split:PackedStringArray = frame_data_csv.split("\n")
+	var frames_split: PackedStringArray = frame_data_csv.split("\n")
 	frames_split = frames_split.slice(1, frames_split.size()) # skip first row of headers
 	
-	var subframe_offset:int = 6 # skip past label, frame_id, num_subframes, rotation_degrees, transparency_type, transparency_flag
-	var subframe_length:int = 8
+	var subframe_offset: int = 6 # skip past label, frame_id, num_subframes, rotation_degrees, transparency_type, transparency_flag
+	var subframe_length: int = 8
 	
-	for frame_text:String in frames_split:
-		var frame_text_split:PackedStringArray = frame_text.split(",")
+	for frame_text: String in frames_split:
+		var frame_text_split: PackedStringArray = frame_text.split(",")
 		
 		if(frame_text[0] == ""):
 			continue
 		
-		var new_frame:FrameData = FrameData.new()
+		var new_frame: FrameData = FrameData.new()
 		
 		new_frame.num_subframes = frame_text_split[2] as int
 		new_frame.y_rotation = frame_text_split[3] as float
@@ -512,7 +512,7 @@ func set_frames_from_csv(filepath:String) -> void:
 		#var frame_data = [num_subframe, rotation_degrees]
 		
 		for i in new_frame.num_subframes:
-			var new_subframe:SubFrameData = SubFrameData.new()
+			var new_subframe: SubFrameData = SubFrameData.new()
 			
 			new_subframe.shift_x = frame_text_split[subframe_offset + (subframe_length*i)] as int
 			new_subframe.shift_y = frame_text_split[subframe_offset + 1 + (subframe_length*i)] as int
@@ -530,17 +530,17 @@ func set_frames_from_csv(filepath:String) -> void:
 		# print(frame_data)
 
 
-func set_zero_frames_from_csv(filepath:String) -> void:
+func set_zero_frames_from_csv(filepath: String) -> void:
 	zero_frames.clear()
 	
 	var file := FileAccess.open(filepath, FileAccess.READ)
 	if file == null:
 		push_warning(FileAccess.get_open_error())
 		return
-	var frame_offsets_data_csv:String = file.get_as_text()
+	var frame_offsets_data_csv: String = file.get_as_text()
 	
 	if file_name == "default_file_name":
-		var new_file_name:String = filepath.get_file()
+		var new_file_name: String = filepath.get_file()
 		new_file_name = new_file_name.trim_suffix(".csv")
 		new_file_name = new_file_name.trim_suffix(".txt")
 		new_file_name = new_file_name.trim_prefix("frame_offset_data_")
@@ -549,15 +549,15 @@ func set_zero_frames_from_csv(filepath:String) -> void:
 		file_name = new_file_name
 		name_alias = new_file_name
 	
-	var offsets_split:PackedStringArray = frame_offsets_data_csv.split("\n")
+	var offsets_split: PackedStringArray = frame_offsets_data_csv.split("\n")
 	offsets_split = offsets_split[1].split(",")
-	for offset:String in offsets_split:
+	for offset: String in offsets_split:
 		zero_frames.append(offset as int)
 	pass
 
 
 func write_csvs() -> void:
-	var headers:PackedStringArray = [
+	var headers: PackedStringArray = [
 		"label",
 		"frame_id",
 		"num_subframes",
@@ -573,12 +573,12 @@ func write_csvs() -> void:
 		"flip_x",
 		"flip_y",
 	]
-	var output:String = ",".join(headers)
+	var output: String = ",".join(headers)
 	var frame_data_string:String = ""
 	
-	var frame_id:int = 0
-	for frame:FrameData in frames:
-		var frame_data:PackedStringArray = [
+	var frame_id: int = 0
+	for frame: FrameData in frames:
+		var frame_data: PackedStringArray = [
 			name_alias,
 			str(frame_id),
 			str(frame.num_subframes),
@@ -588,8 +588,8 @@ func write_csvs() -> void:
 		]
 		frame_data_string = ",".join(frame_data)
 		
-		for subframe:SubFrameData in frame.subframes:
-			var text_parts:PackedStringArray = [
+		for subframe: SubFrameData in frame.subframes:
+			var text_parts: PackedStringArray = [
 				frame_data_string,
 				str(subframe.shift_x),
 				str(subframe.shift_y),
@@ -602,15 +602,15 @@ func write_csvs() -> void:
 			
 			frame_data_string = ",".join(text_parts);
 		
-		var allText:PackedStringArray = [output, frame_data_string];
+		var allText: PackedStringArray = [output, frame_data_string];
 		output = "\n".join(allText)
 		
 		frame_id += 1
 	
 	# submerged frames
-	var frame_id_submerged:int = 0
-	for frame:FrameData in frames_submerged:
-		var frame_data:PackedStringArray = [
+	var frame_id_submerged: int = 0
+	for frame: FrameData in frames_submerged:
+		var frame_data: PackedStringArray = [
 			name_alias + "-submerged",
 			str(frame_id_submerged),
 			str(frame.num_subframes),
@@ -620,8 +620,8 @@ func write_csvs() -> void:
 		]
 		frame_data_string = ",".join(frame_data)
 		
-		for subframe:SubFrameData in frame.subframes:
-			var text_parts:PackedStringArray = [
+		for subframe: SubFrameData in frame.subframes:
+			var text_parts: PackedStringArray = [
 				frame_data_string,
 				str(subframe.shift_x),
 				str(subframe.shift_y),
@@ -634,7 +634,7 @@ func write_csvs() -> void:
 			
 			frame_data_string = ",".join(text_parts);
 		
-		var allText:PackedStringArray = [output, frame_data_string];
+		var allText: PackedStringArray = [output, frame_data_string];
 		output = "\n".join(allText)
 		
 		frame_id_submerged += 1
@@ -643,8 +643,8 @@ func write_csvs() -> void:
 	var save_file := FileAccess.open("user://FFTae/frame_data_"+file_name+".txt", FileAccess.WRITE)
 	save_file.store_string(output)
 	
-	if (file_name.begins_with("wep") || file_name.begins_with("eff")):		
-		var frame_offset_headers:PackedStringArray = [
+	if (file_name.begins_with("wep") || file_name.begins_with("eff")):
+		var frame_offset_headers: PackedStringArray = [
 			"Knife",
 			"Ninja Sword",
 			"Sword",

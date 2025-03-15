61891bc58443a588262916a98776c540c66c52b0
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-03-15 11:06:26 -0400

    fixed controls to be relative to camera facing

--- src/Unit/character_controller_realtime.gd
+++ src/Unit/character_controller_realtime.gd
@@ -50,7 +50,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (unit.char_body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		unit.char_body.velocity.x = direction.x * SPEED
 		unit.char_body.velocity.z = direction.z * SPEED

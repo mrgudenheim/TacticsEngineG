df4a15e1c86f64f08c4ec87d1c0e340a34d1f1cf
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-03-16 19:09:05 -0400

    fixed unit to face camera in FFTae

--- src/fftae/preview_manager.gd
+++ src/fftae/preview_manager.gd
@@ -52,6 +52,8 @@ func add_unit() -> void:
 	var new_unit: UnitData = unit_tscn.instantiate()
 	preview_viewport.subviewport.add_child(new_unit)
 	new_unit.initialize_unit()
+	new_unit.char_body.rotation_degrees = Vector3.ZERO
+	new_unit.animation_manager.rotation_degrees = Vector3.ZERO
 	unit = new_unit
 	preview_viewport.camera_control.sprite = unit.animation_manager.unit_sprites_manager.sprite_primary
 

2016caf299cdbf95b43a1e201f00d78e297e7974
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-03-17 07:00:38 -0400

    fixed camera placement and rotation so unit movement is apparent

--- src/fftae/preview_manager.gd
+++ src/fftae/preview_manager.gd
@@ -54,7 +54,7 @@ func add_unit() -> void:
 	#preview_viewport.subviewport.add_child(new_unit)
 	preview_viewport2.add_child(new_unit)
 	new_unit.initialize_unit()
-	new_unit.char_body.rotation_degrees = Vector3.ZERO
+	#new_unit.char_body.rotation_degrees = Vector3.ZERO
 	new_unit.animation_manager.rotation_degrees = Vector3.ZERO
 	unit = new_unit
 	#preview_viewport.camera_control.sprite = unit.animation_manager.unit_sprites_manager.sprite_primary

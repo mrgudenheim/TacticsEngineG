efd55af0eaa4cac57a3d596bfcb778573bc19a49
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-05-17 09:10:18 -0400

    fix showing action potential targets

--- src/actions/action_button.gd
+++ src/actions/action_button.gd
@@ -10,9 +10,20 @@ func _init(new_action_instance: ActionInstance) -> void:
 	name = action_instance.action.action_name
 	
 	pressed.connect(action_instance.start_targeting)
-	focus_entered.connect(action_instance.user.active_action.stop_targeting)
-	focus_entered.connect(action_instance.show_potential_targets)
 	
-	focus_exited.connect(action_instance.hide_potential_targets)
-	focus_exited.connect(action_instance.user.active_action.start_targeting)
+	focus_entered.connect(show_potential_targets)
+	focus_exited.connect(hide_potential_targets)
+
+
+func show_potential_targets() -> void:
+	if is_instance_valid(action_instance.user.active_action):
+		action_instance.user.active_action.stop_targeting()
 	
+	action_instance.show_potential_targets()
+
+
+func hide_potential_targets() -> void:
+	action_instance.hide_potential_targets()
+	
+	if is_instance_valid(action_instance.user.active_action):
+		action_instance.user.active_action.start_targeting()

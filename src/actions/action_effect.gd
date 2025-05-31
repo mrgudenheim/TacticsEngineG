16cac018644e14a472146aa15f7f0f8e49049176
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-05-30 20:11:27 -0400

    implemented action effect types

--- src/actions/action_effect.gd
+++ src/actions/action_effect.gd
@@ -23,7 +23,7 @@ enum EffectType {
 	LEVEL,
 	CURRENCY,
 	INVENTORY,
-	BREAK_EQUIPMENT,
+	#BREAK_EQUIPMENT, # Break is Remove equipment + lower inventory?
 	REMOVE_EQUIPMENT, # Steal if transfer = true
 	#PE,
 	#ME,
@@ -38,9 +38,83 @@ func _init(new_type: EffectType, new_show_ui: bool = true, new_transfer_to_user:
 
 
 func apply(user: UnitData, target: UnitData, value: int) -> int:
+	var unit: UnitData = target
+	if apply_to_user:
+		unit = user
+	
 	match type:
 		EffectType.HP:
-			target.hp_current += value
+			if set_value:
+				unit.hp_current = value
+			else:
+				unit.hp_current += value
+		EffectType.MP:
+			if set_value:
+				unit.mp_current = value
+			else:
+				unit.mp_current += value
+		EffectType.CT:
+			if set_value:
+				unit.ct_current = value
+			else:
+				unit.ct_current += value
+		EffectType.MOVE:
+			if set_value:
+				unit.move_current = value
+			else:
+				unit.move_current += value
+		EffectType.JUMP:
+			if set_value:
+				unit.jump_current = value
+			else:
+				unit.jump_current += value
+		EffectType.SPEED:
+			if set_value:
+				unit.speed_current = value
+			else:
+				unit.speed_current += value
+		EffectType.PHYSICAL_ATTACK: # TODO way to modify MA
+			if set_value:
+				unit.hp_current = value
+			else:
+				unit.hp_current += value
+		EffectType.MAGIC_ATTACK: # TODO way to modify MA
+			if set_value:
+				unit.hp_current = value
+			else:
+				unit.hp_current += value
+		EffectType.BRAVE:
+			if set_value:
+				unit.brave_current = value
+			else:
+				unit.brave_current += value
+		EffectType.FAITH:
+			if set_value:
+				unit.faith_current = value
+			else:
+				unit.faith_current += value
+		EffectType.EXP:
+			if set_value:
+				unit.unit_exp = value
+			else:
+				unit.unit_exp += value
+		EffectType.LEVEL:
+			if set_value:
+				unit.level = value
+			else:
+				unit.level += value
+		EffectType.CURRENCY:
+			if set_value:
+				unit.team.currency = value
+			else:
+				unit.team.currency += value
+		EffectType.INVENTORY:
+			if set_value:
+				unit.team.inventory[0] = value # TODO get inventory item id to change
+			else:
+				unit.team.inventory[0] += value # TODO get inventory item id to change
+		EffectType.REMOVE_EQUIPMENT:
+			unit.change_equipment(0, 0) # TODO get equipment slot id to change
 	
 	return value
 	

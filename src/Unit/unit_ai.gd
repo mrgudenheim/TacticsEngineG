2aa179a1e97f47c2ec7154065ddebfc3da03951f
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-06-28 20:12:02 -0400

    added placeholder unit ai class

---
+++ src/Unit/unit_ai.gd
@@ -0,0 +1,8 @@
+#https://ffhacktics.com/wiki/BATTLE.BIN_Routines#AI_Calculations
+
+#https://ffhacktics.com/wiki/AI_Ability_Use_Decisions
+#https://ffhacktics.com/wiki/Evaluate_Cancel_Status_Ability_(0019881c)
+#https://ffhacktics.com/wiki/Find_Peril_Most_Unit_(00198b04)
+
+class_name UnitAi
+extends Resource

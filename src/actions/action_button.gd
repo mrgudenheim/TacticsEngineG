b9033b31084aa246b0e5c9b8366dc8e975e82c85
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-07-04 00:56:11 -0400

    fix action button targeting preview

--- src/actions/action_button.gd
+++ src/actions/action_button.gd
@@ -11,8 +11,11 @@ func _init(new_action_instance: ActionInstance) -> void:
 	
 	pressed.connect(action_instance.start_targeting)
 	
-	focus_entered.connect(show_potential_targets)
-	focus_exited.connect(hide_potential_targets)
+	focus_entered.connect(set_active_action)
+	#focus_exited.connect(hide_potential_targets)
+	
+	mouse_entered.connect(show_potential_targets)
+	mouse_exited.connect(hide_potential_targets)
 
 
 func show_potential_targets() -> void:
@@ -25,5 +28,21 @@ func show_potential_targets() -> void:
 func hide_potential_targets() -> void:
 	action_instance.hide_potential_targets()
 	
-	if is_instance_valid(action_instance.user.active_action):
-		action_instance.user.active_action.start_targeting()
+	if action_instance.user.active_action != null:
+		if not action_instance.user.active_action.action.auto_target:
+			action_instance.user.active_action.start_targeting()
+
+
+func set_active_action() -> void:
+	if disabled:
+		return
+	
+	if not action_instance.action.auto_target:
+		if not action_instance.potential_targets_are_set:
+			action_instance.update_potential_targets()
+		action_instance.start_targeting()
+	else:
+		if action_instance.user.active_action != null:
+			action_instance.user.active_action.stop_targeting()
+		action_instance.show_potential_targets()
+		action_instance.user.active_action = action_instance

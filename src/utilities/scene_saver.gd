1aa81b4b1bd9858229b1f50c7b212e4563d076de
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-07-12 18:41:06 -0400

    move overrides to user data

--- src/utilities/scene_saver.gd
+++ src/utilities/scene_saver.gd
@@ -17,7 +17,9 @@ func save_scene() -> void:
 	
 	node_to_save.material_override = null
 	
-	var directory: String = "res://overrides/MAP/"
+	var directory: String = "user://overrides/MAP/"
+	DirAccess.make_dir_recursive_absolute(directory)
+	
 	var file_name: String = node_to_save.name
 	
 	#ResourceSaver.save(scene, directory + "MyScene.tscn")
@@ -33,11 +35,11 @@ func save_scene() -> void:
 	var extension: String = ".glb"
 	gltf_document.write_to_filesystem(gltf_state, directory + file_name + extension)
 	
-	push_warning(gltf_state.base_path)
+	push_warning("Saved: " + directory + file_name + extension)
 
 
 func import_gltf(file_name: String) -> Node:
-	var directory: String = "res://overrides/MAP/"
+	var directory: String = "user://overrides/MAP/"
 	
 	var gltf_state: GLTFState = GLTFState.new()
 	#gltf_state.base_path = directory

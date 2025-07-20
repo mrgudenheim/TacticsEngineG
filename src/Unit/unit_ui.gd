a46e078127195051849c837a25e64c7bca5d01e3
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-07-20 10:47:18 -0400

    wait resource updated based on action update to use Array[int] for statuses

--- src/Unit/unit_ui.gd
+++ src/Unit/unit_ui.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	camera = get_viewport().get_camera_3d()
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if camera != null:
 		var camera_right: Vector3 = camera.basis * Vector3.RIGHT
 		position = camera.unproject_position(unit_char_body.position + (Vector3.UP * offset.y) + (camera_right * offset.x))

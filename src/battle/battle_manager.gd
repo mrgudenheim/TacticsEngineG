cc62f459f19b79f7c2a40f68255439c483dbf019
mrgudenheim <bgudenius1@gmail.com>
2025-09-26 21:36:18 -0400

    remove test name

--- src/battle/battle_manager.gd
+++ src/battle/battle_manager.gd
@@ -359,7 +359,7 @@ func add_test_teams_to_map() -> void:
 	# test_ability.triggered_actions = [test_triggered_action]
 	
 	test_ability.triggered_actions_names.append("attack")
-	test_ability.triggered_actions_names.append("regen_heal")
+	# test_ability.triggered_actions_names.append("regen_heal")
 	test_ability.display_name = "Counter Attack"
 	test_ability.unique_name = "counter"
 	Utilities.save_json(test_ability)

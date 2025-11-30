29e3d0b9e412109b5600024cde617a87259e25a1
mrgudenheim <bgudenius1@gmail.com>
2025-11-29 19:39:05 -0500

    rearrange unit battle details ui mockup add references to ui elements

---
+++ src/Unit/unit_details_battle_ui.gd
@@ -0,0 +1,27 @@
+class_name UnitDetailsBattleUi
+extends PanelContainer
+
+@export var portrait_rect: TextureRect
+@export var team_label: Label
+@export var unit_name_label: Label
+@export var level_label: Label
+
+@export var hp_bar: StatBar
+@export var mp_bar: StatBar
+@export var ct_bar: StatBar
+
+@export var pa_label: Label
+@export var ma_label: Label
+@export var brave_label: Label
+@export var faith_label: Label
+@export var move_label: Label
+@export var jump_label: Label
+@export var speed_label: Label
+
+@export var evade_grid: GridContainer
+@export var equipment_grid: GridContainer
+@export var ability_grid: GridContainer
+
+@export var current_status_list: VBoxContainer
+@export var immune_status_list: VBoxContainer
+@export var element_affinity_list: VBoxContainer

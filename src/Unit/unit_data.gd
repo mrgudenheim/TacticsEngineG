753d62a49a94db869483830980e9df0f4e1d73a9
mrgudenheim <bgudenius1@gmail.com>
2026-01-31 12:21:25 -0500

    mark unit data vars as @export

--- src/Unit/unit_data.gd
+++ src/Unit/unit_data.gd
@@ -3,13 +3,13 @@ extends Resource
 
 @export var display_name: String = "display name"
 @export var level: int = 0
-var gender: String = "gender"
-var zodiac: String = "zodiac" # TODO should zodiac be derived from birthday?
-var job_unique_name: String = "job_unique_name"
-var team: int = 0
-var controller: int = 0 # 0 = AI, 1 = Player 1, etc.
-var sprite_unique_name: String = "sprite_unique_name"
-var palette_id: int = 0
+@export var gender: String = "gender"
+@export var zodiac: String = "zodiac" # TODO should zodiac be derived from birthday?
+@export var job_unique_name: String = "job_unique_name"
+@export var team: int = 0
+@export var controller: int = 0 # 0 = AI, 1 = Player 1, etc.
+@export var sprite_unique_name: String = "sprite_unique_name"
+@export var palette_id: int = 0
 
 # Stats
 

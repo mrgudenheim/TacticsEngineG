795966040d90360153d8776524f6af98284d2224
mrgudenheim <bgudenius1@gmail.com>
2026-01-31 15:32:22 -0500

    add initial functions to be used for saving UnitData

--- src/Unit/unit_data.gd
+++ src/Unit/unit_data.gd
@@ -26,3 +26,37 @@ extends Resource
 # job levels
 # jp per job
 # abilities learned
+
+
+static func create_from_dictionary(property_dict: Dictionary) -> UnitData:
+	var new_unit_data: UnitData = UnitData.new()
+	for property_name in property_dict.keys():
+		# if property_name == "corner_position":
+		# 	var vector_as_array = property_dict[property_name]
+		# 	var new_corner_position: Vector3i = Vector3i(roundi(vector_as_array[0]), roundi(vector_as_array[1]), roundi(vector_as_array[2]))
+		# 	new_unit_data.set(property_name, new_corner_position)
+		# elif property_name == "mirror_xyz":
+		# 	var array = property_dict[property_name]
+		# 	var new_mirror_xyz: Array[bool] = []
+		# 	new_mirror_xyz.assign(array)
+		# 	new_unit_data.set(property_name, new_mirror_xyz)
+		# else:
+			new_unit_data.set(property_name, property_dict[property_name])
+
+	new_unit_data.emit_changed()
+	return new_unit_data
+
+
+func to_dictionary() -> Dictionary:
+	var properties_to_exclude: PackedStringArray = [
+		"RefCounted",
+		"Resource",
+		"resource_local_to_scene",
+		"resource_path",
+		"resource_name",
+		"resource_scene_unique_id",
+		"script",
+	]
+	
+	return Utilities.object_properties_to_dictionary(self, properties_to_exclude)
+

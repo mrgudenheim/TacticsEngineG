389206873c269c3e2cdbf2e6d80f78678782c920
mrgudenheim <bgudenius1@gmail.com>
2026-07-29 08:18:41 -0400

    improve clarity of importing gltf mesh

--- src/utilities/gltf_manager.gd
+++ src/utilities/gltf_manager.gd
@@ -31,13 +31,13 @@ static func import_gltf_mesh(import_path: String) -> MeshInstance3D:
 	var gltf_state: GLTFState = GLTFState.new()
 	var gltf_document: GLTFDocument = GLTFDocument.new()
 	var error: int = gltf_document.append_from_file(import_path, gltf_state, 0, import_path.get_base_dir())
-	if error != 0:
-		push_warning(import_path.get_file() + " failed to import as glb: " + str(error))
+	if error != OK:
+		push_warning(import_path.get_file() + " failed to import as glb: " + error_string(error))
 		return null
 	
-	var mesh_resource: ImporterMesh = gltf_state.get_meshes()[0].mesh
+	var gltf_mesh_resource: ImporterMesh = gltf_state.get_meshes()[0].mesh
 	var mesh_instance: MeshInstance3D = MeshInstance3D.new()
-	mesh_instance.mesh = mesh_resource.get_mesh()
+	mesh_instance.mesh = gltf_mesh_resource.get_mesh()
 	mesh_instance.name = import_path.get_file().get_basename()
 	
 	return mesh_instance

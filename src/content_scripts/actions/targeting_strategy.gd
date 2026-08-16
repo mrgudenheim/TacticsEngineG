e9ff742276d0f2d9393ee61302452c573fdcdcbe
mrgudenheim <bgudenius1@gmail.com>
2026-08-16 07:44:00 -0400

    fix warnings about unused parameters

--- src/content_scripts/actions/targeting_strategy.gd
+++ src/content_scripts/actions/targeting_strategy.gd
@@ -2,12 +2,12 @@
 class_name TargetingStrategy
 extends Resource
 
-func get_potential_targets(action_instance: ActionInstance) -> Array[TerrainTile]:
+func get_potential_targets(_action_instance: ActionInstance) -> Array[TerrainTile]:
 	push_error("Using base TargetingStrategy instead of specific targeting strategy")
 	return []
 
 
-func get_aoe_targets(action_instance: ActionInstance, tile_target: TerrainTile) -> Array[TerrainTile]:
+func get_aoe_targets(_action_instance: ActionInstance, _tile_target: TerrainTile) -> Array[TerrainTile]:
 	push_error("Using base TargetingStrategy get_aoe_targets instead of specific targeting strategy")
 	return []
 
@@ -16,5 +16,5 @@ func start_targeting(action_instance: ActionInstance) -> void:
 	action_instance.show_targets_highlights(action_instance.potential_targets_highlights)
 
 
-func stop_targeting(action_instance: ActionInstance) -> void:
+func stop_targeting(_action_instance: ActionInstance) -> void:
 	push_error("Using base TargetingStrategy instead of specific targeting strategy")
